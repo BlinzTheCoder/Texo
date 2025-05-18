2d736d870297e01dc542af016cbd3de742bbdff8
Blinz <145363040+BlinzTheCoder@users.noreply.github.com>
2025-05-18 17:00:48 +0530

    Updated (Again)

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,20 @@
-import cloudflare from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
-  kit: {
-    adapter: cloudflare(),
-    paths: {}
-  }
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			config: undefined,
+			platformProxy: {
+				configPath: undefined,
+				environment: undefined,
+				persist: undefined
+			},
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
